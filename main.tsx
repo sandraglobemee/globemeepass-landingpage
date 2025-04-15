b19c98f83f9f32cd99625de1990043146e590987
sandraglobemee <sandra.schmitt@globemee.com>
2025-04-15 11:35:34 +0200

    Create main.tsx

---
+++ main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./home"; // Falls deine Datei in src/home.tsx liegt
+
+const rootElement = document.getElementById("root")!;
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>
+);
