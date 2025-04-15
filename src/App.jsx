8cde6be2614998c77c6cbee5608ef7de7a82b08f
sandraglobemee <sandra.schmitt@globemee.com>
2025-04-15 12:29:28 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>GlobemeePass Landingpage</h1>
+      <p>Willkommen auf unserer Seite!</p>
+    </div>
+  );
+}
+
+export default App;
